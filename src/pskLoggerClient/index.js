03488b04a87c8e765c78a982ffa74d1a083c3467
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-05-10 13:27:08 +0300

    Added very basic PSKLogger

---
+++ src/pskLoggerClient/index.js
@@ -0,0 +1,51 @@
+const LoggerClient = require('../LoggerClient').LoggerClient;
+const TransportClient = require('../TransportClient').TransportClient;
+
+
+function PSKLogger () {
+    const transport = new TransportClient('tcp://127.0.0.1:7777');
+    const logger = new LoggerClient(transport);
+
+    function debug(...params) {
+        const meta = prepareMeta();
+        logger.debug(meta, ...params);
+    }
+
+    function error(...params) {
+        const meta = prepareMeta();
+        logger.error(meta, ...params);
+    }
+
+    function info(...params) {
+        const meta = prepareMeta();
+        logger.info(meta, ...params);
+    }
+
+    function log(...params) {
+        const meta = prepareMeta();
+        logger.log(meta, ...params);
+    }
+
+    function warn(...params) {
+        const meta = prepareMeta();
+        logger.warn(meta, ...params);
+    }
+
+    function prepareMeta() {
+        const processPath = process.argv[1];
+        const processRelativePath = processPath.split('/modules/', 2)[1];
+        return {
+            processPath: processRelativePath
+        };
+    }
+
+
+    this.debug = debug;
+    this.error = error;
+    this.info = info;
+    this.log = log;
+    this.warn = warn;
+
+}
+
+module.exports = PSKLogger;

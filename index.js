20b3c4a65a85032342ba511a9a576ecdeb372fc6
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-05-15 10:53:21 +0300

    Added method to overwrite console

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 const PSKLogger = require('./src/pskLoggerClient/index');
 
-module.exports = {PSKLogger};
+function overwriteConsole() {
+    const logger = new PSKLogger();
+
+    const originalConsole = {};
+    Object.keys(console).forEach(key => originalConsole[key] = console[key]);
+
+    Object.keys(logger).forEach(key => {
+       console[key] = function() {
+           const log = logger[key].apply(logger, arguments);
+
+           originalConsole[key].apply(originalConsole, ['[logger]', ...log.messages]);
+       }
+    });
+}
+
+module.exports = {
+    overwriteConsole,
+    PSKLogger
+};

bbaa3af228249b2c04f1cb5901839444e55ddd60
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-06-03 09:44:51 +0300

    Added platform to log environment data

--- src/utils/EnvironmentDataProvider.js
+++ src/utils/EnvironmentDataProvider.js
@@ -1,12 +1,16 @@
 const path = require('path');
+const os = require('os');
+
+const platform = os.platform();
 
 function getEnvironmentData () {
     const processPath = process.argv[1];
     const lastSep = processPath.lastIndexOf(path.sep);
     const processStartFile = processPath.substring(lastSep + 1);
     return {
-        context: `node/${processStartFile}`,
-        processStartFile: processStartFile
+        context: `node:${processStartFile}`,
+        processStartFile: processStartFile,
+        platform: platform
     };
 }
 

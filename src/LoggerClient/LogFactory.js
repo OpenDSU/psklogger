737c5084b7fd845bfddd430c1625e72d9a2cd341
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-07-22 14:13:25 +0300

    Added millisecond time to logs

--- src/LoggerClient/LogFactory.js
+++ src/LoggerClient/LogFactory.js
@@ -1,10 +1,13 @@
+const {performance} = require('perf_hooks');
+
 class LogFactory {
     static createLog(logLevel, meta, messages) {
         return {
             level: logLevel,
             messages: messages,
             meta: meta,
-            time: new Date()
+            time: new Date(),
+            msTime: performance.now() + performance.timeOrigin
         }
     }
 }

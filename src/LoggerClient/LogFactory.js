19ed225862f0b8664b682ca11b4fa5d98504c41f
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-07-23 14:00:22 +0300

    Refactoring LogFactory for simplicity

--- src/LoggerClient/LogFactory.js
+++ src/LoggerClient/LogFactory.js
@@ -1,16 +1,15 @@
 const {performance} = require('perf_hooks');
 
-class LogFactory {
-    static createLog(logLevel, meta, messages) {
-        return {
-            level: logLevel,
-            messages: messages,
-            meta: meta,
-            time: new Date(),
-            msTime: performance.now() + performance.timeOrigin
-        }
+function createLog(logLevel, meta, messages) {
+    return {
+        level: logLevel,
+        messages: messages,
+        meta: meta,
+        time: new Date(),
+        msTime: performance.now() + performance.timeOrigin
     }
 }
 
-
-module.exports = LogFactory;
+module.exports = {
+    createLog
+};
